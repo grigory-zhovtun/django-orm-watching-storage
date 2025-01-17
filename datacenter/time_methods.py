46c0b367a090ad15bd5a6bc2a87a892a73cf02f4
grigory-zhovtun <grigory.zhovtun@gmail.com>
2025-01-17 17:46:21 +0500

    add time constants instead "magic numbers", for more clear code

--- datacenter/time_methods.py
+++ datacenter/time_methods.py
@@ -1,6 +1,11 @@
 from datetime import timedelta, datetime
 
 
+SECONDS_IN_DAY = 86400
+SECONDS_IN_HOUR = 3600
+SECONDS_IN_MINUTE = 60
+
+
 def is_visit_long(visit, minutes=60):
     visit_duration = visit.leaved_at - visit.entered_at
 
@@ -9,20 +14,21 @@ def is_visit_long(visit, minutes=60):
     else:
         return False
 
+
 def get_duration(spent_time):
     if not isinstance(spent_time, timedelta):
         raise ValueError("Input must be a timedelta object")
     total_seconds = int(spent_time.total_seconds())
-    hours, remainder = divmod(total_seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
+    hours, remainder = divmod(total_seconds, SECONDS_IN_HOUR)
+    minutes, seconds = divmod(remainder, SECONDS_IN_MINUTE)
     return f"{hours:02}:{minutes:02}:{seconds:02}"
 
 
 def format_duration(duration):
     total_seconds = int(duration.total_seconds())
-    days, remainder = divmod(total_seconds, 86400)
-    hours, remainder = divmod(remainder, 3600)
-    minutes, seconds = divmod(remainder, 60)
+    days, remainder = divmod(total_seconds, SECONDS_IN_DAY)
+    hours, remainder = divmod(remainder, SECONDS_IN_HOUR)
+    minutes, seconds = divmod(remainder, SECONDS_IN_MINUTE)
 
     result = []
     if days > 0:
